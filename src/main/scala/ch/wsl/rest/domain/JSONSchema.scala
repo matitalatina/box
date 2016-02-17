595afeb1c0243644de2e8274db3b2541a79af8ed
Andrea Minetti <andrea.minetti@wavein.ch>
2016-02-17 11:32:49 +0100

    Slickly everything

--- src/main/scala/ch/wsl/rest/domain/JSONSchema.scala
+++ src/main/scala/ch/wsl/rest/domain/JSONSchema.scala
@@ -21,11 +21,11 @@ object JSONSchema {
   import StringHelper._
 
   def of(table:String,db:slick.driver.PostgresDriver.api.Database):Future[JSONSchema] = {
-    
+
     val schema = new PgSchema(table,db)
     
     val map = schema.columns.map{ c => ListMap(properties(c): _*) }
-    
+
     println(schema.columns)
 
     for{
@@ -38,7 +38,7 @@ object JSONSchema {
         title = Some(table),
         properties = Some(m),
         readonly = Some(c.forall { x => x.is_updatable == "NO" }),
-        required = Some(c.filter(_.is_nullable == "NO").map(_.column_name))
+        required = Some(c.filter(_.is_nullable == "NO").map(_.column_name.slickfy))
       )
     }
   }
@@ -53,7 +53,7 @@ object JSONSchema {
     val cols = {for{
       c <- columns
     } yield {
-      c.column_name.slickfy -> JSONSchema(typesMapping(c.data_type),Some(c.column_name),order=Some(c.ordinal_position),readonly=Some(c.is_updatable == "NO"))
+      c.column_name.slickfy -> JSONSchema(typesMapping(c.data_type),Some(c.column_name.slickfy),order=Some(c.ordinal_position),readonly=Some(c.is_updatable == "NO"))
     }}.toList
     
     
