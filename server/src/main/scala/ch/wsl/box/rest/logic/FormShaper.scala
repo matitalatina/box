b64533e0c6143fbf10b506ad78cd7338e6023dd4
Andrea Minetti <andrea.minetti@wavein.ch>
2017-05-19 15:34:22 +0200

    FormShaper implementation for getting should be ok

--- server/src/main/scala/ch/wsl/box/rest/logic/FormShaper.scala
+++ server/src/main/scala/ch/wsl/box/rest/logic/FormShaper.scala
@@ -3,16 +3,13 @@ package ch.wsl.box.rest.logic
 import io.circe._
 import io.circe.syntax._
 import ch.wsl.box.model.shared._
-import ch.wsl.box.model.tables
 import ch.wsl.box.model.TablesRegistry
-import ch.wsl.box.model.tables.{Canton, Canton_row}
 import io.circe.Json
 import slick.lifted.Query
 import slick.driver.PostgresDriver.api._
 
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.reflect.ClassTag
 
 /**
   * Created by andre on 5/18/2017.
@@ -27,13 +24,17 @@ case class FormShaper[T <: slick.driver.PostgresDriver.api.Table[M],M <: Product
 
 
   private def createQuery(model:Json,subform: Subform):JSONQuery = {
-    //subform.localFields.split(",").zip(subform.subField.split(","))
-    //JSONQueryFilter()
-    JSONQuery(50,0,List(),List())
+    val filters = for{
+      (local,remote) <- subform.localFields.split(",").zip(subform.subFields.split(","))
+    } yield {
+      JSONQueryFilter(remote,Some(Filter.EQUALS),model.hcursor.get[Json](local).right.get.toString())
+    }
+    JSONQuery(50,0,List(),filters.toList)
   }
 
-  private def getSubform(field:JSONField,form:JSONForm,subform:Subform):Future[Seq[Json]] = {
-    ??? //TablesRegistry.actions(form.table).getModel(field.)
+  private def getSubform(model:Json, field:JSONField,form:JSONForm,subform:Subform):Future[Seq[Json]] = {
+    val query = createQuery(model,subform)
+    TablesRegistry.actions(form.table).getModel(query)
   }
 
   private def toJson(model:M):Future[Json] = {
@@ -43,7 +44,7 @@ case class FormShaper[T <: slick.driver.PostgresDriver.api.Table[M],M <: Product
         case None => Future.successful(field.key -> json.hcursor.get[Json](field.key).right.get)
         case Some(subform) => for{
           form <- Forms(subform.id,form.lang)
-          result <- getSubform(field,form,subform)
+          result <- getSubform(json,field,form,subform)
         } yield field.key -> result.asJson
       }
     }
