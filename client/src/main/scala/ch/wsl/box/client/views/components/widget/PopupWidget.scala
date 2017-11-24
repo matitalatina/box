b8873c1f5483051512e697ac2d637fa68a7c09a0
Andrea Minetti <andrea.minetti@wavein.ch>
2017-11-24 17:46:18 +0100

    popup widget

--- client/src/main/scala/ch/wsl/box/client/views/components/widget/PopupWidget.scala
+++ client/src/main/scala/ch/wsl/box/client/views/components/widget/PopupWidget.scala
@@ -7,20 +7,99 @@ import io.udash._
 import io.udash.properties.single.Property
 import org.scalajs.dom.Event
 import ch.wsl.box.shared.utils.JsonUtils._
+import io.udash.bootstrap.BootstrapStyles
+import io.udash.bootstrap.button.{ButtonStyle, UdashButton, UdashButtonGroup}
+import io.udash.bootstrap.modal.{ModalSize, UdashModal}
+import org.scalajs.dom
+import scala.concurrent.duration._
 
-import scalacss.ScalatagsCss._
-import scalatags.JsDom.all.{label => lab}
-import scalatags.JsDom.all._
+
 
 case class PopupWidget(options:JSONFieldOptions,label: String, prop: Property[Json]) extends OptionWidget {
+
+import ch.wsl.box.client.Context._
+  import scalacss.ScalatagsCss._
+  import scalatags.JsDom.all.{label => lab}
+  import scalatags.JsDom.all._
+
+
+  val sortedOptions = options.options.toSeq.sortBy(_._2)
+
   override def render() = {
 
 
+
+
+    object Status{
+      val Closed = "closed"
+      val Open = "open"
+    }
+
+    val searchProp = Property("")
+
+    val modalStatus = Property(Status.Closed)
+
     val selectModel: Property[String] = prop.transform(value2Label,label2Value)
 
+
+
+    val header = () => div(
+      label,
+      UdashButton()(
+        UdashModal.CloseButtonAttr,
+        BootstrapStyles.close, "×"
+      ).render
+    ).render
+
+    val optionList:Modifier = div(
+      lab("Search"),br,
+      TextInput(searchProp,Some(500 milliseconds)),br,br,
+      produce(searchProp) { searchTerm =>
+        div(
+          sortedOptions.filter(opt => searchTerm == "" || opt._2.toLowerCase.contains(searchTerm.toLowerCase)).map { case (key, value) =>
+            li(a(value, onclick :+= ((e: Event) => {
+              modalStatus.set(Status.Closed)
+              selectModel.set(value)
+            })))
+          }
+        ).render
+      }
+    )
+
+    val body = () => div(
+      ul(
+        optionList
+      )
+    ).render
+
+    val footer = () => div(
+      button(onclick :+= ((e:Event) => modalStatus.set(Status.Closed),true),"Close")
+    ).render
+
+    val modal:UdashModal = UdashModal(modalSize = ModalSize.Large)(
+      headerFactory = Some(header),
+      bodyFactory = Some(body),
+      footerFactory = Some(footer)
+    )
+
+
+    modalStatus.listen{ state =>
+      println(s"State changed to:$state")
+      state match {
+        case Status.Open => modal.show()
+        case Status.Closed => modal.hide()
+      }
+    }
+
+
+
     div(BootstrapCol.md(12),GlobalStyles.noPadding)(
+      modal.render,
       if(label.length >0) lab(label) else {},
-      span(bind(selectModel),button("Change", onclick :+= ((e:Event) => println(e))))
+      span(
+        bind(selectModel),br,
+        button(onclick :+= ((e:Event) => modalStatus.set(Status.Open),true),"Change")
+      )
     )
   }
 }
