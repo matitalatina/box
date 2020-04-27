ec0ae57ef24652646fb4138f9e4f378661ab2417
Andrea Minetti <andrea.minetti@wavein.ch>
2020-04-27 15:04:23 +0200

    Fixed compilation error

--- server/src/main/scala/ch/wsl/box/model/boxentities/Log.scala
+++ server/src/main/scala/ch/wsl/box/model/boxentities/Log.scala
@@ -23,7 +23,7 @@ object Log {
   class Logs(_tableTag: Tag) extends profile.api.Table[Log_row](_tableTag, "log") {
     def * = (Rep.Some(id), filename, classname, line, message,timestamp) <> (Log_row.tupled, Log_row.unapply)
     /** Maps whole row to an option. Useful for outer joins. */
-    def ? = (Rep.Some(id), filename, classname, line, message,timestamp).shaped.<>({r=>import r._; _1.map(_=> Log_row.tupled((_1, _2, _3, _4, _5)))}, (_:Any) =>  throw new Exception("Inserting into ? projection not supported."))
+    def ? = (Rep.Some(id), filename, classname, line, message,timestamp).shaped.<>({r=>import r._; _1.map(_=> Log_row.tupled((_1, _2, _3, _4, _5, _6)))}, (_:Any) =>  throw new Exception("Inserting into ? projection not supported."))
 
     val id: Rep[Int] = column[Int]("id", O.AutoInc, O.PrimaryKey)
     val filename: Rep[String] = column[String]("filename")
