b487c6d37ece144c8bd003d964f23bfd3335835c
Andrea Minetti <andrea.minetti@wavein.ch>
2021-01-29 15:42:03 +0100

    Fix #125 line breaks

--- client/src/main/scala/ch/wsl/box/client/views/components/widget/RichTextEditorWidget.scala
+++ client/src/main/scala/ch/wsl/box/client/views/components/widget/RichTextEditorWidget.scala
@@ -13,8 +13,9 @@ import io.udash.properties.single.Property
 import org.scalablytyped.runtime.StringDictionary
 import scalatags.JsDom
 import scribe.Logging
-import typings.quill.mod.{Delta, DeltaOperation, DeltaStatic, QuillOptionsStatic, Sources}
+import typings.quill.mod.{DeltaStatic, Quill, QuillOptionsStatic, Sources}
 
+import scala.collection.mutable
 import scala.scalajs.js
 import scala.util.Try
 
@@ -57,16 +58,95 @@ case class RichTextEditorWidget(_id: Property[Option[String]], field: JSONField,
       val parent = div(container).render
 
 
+      //Line break fix inspired by:
+      // https://codepen.io/farnabaz/pen/VpdaGW
+      // https://codepen.io/mackermedia/pen/gmNwZP
 
-      val editor = new typings.quill.mod.default(container
-        ,QuillOptionsStatic()
+      val Parchment = typings.quill.mod.default.`import`("parchment").asInstanceOf[js.Dynamic]
+      val Delta = typings.quill.mod.default.`import`("delta").asInstanceOf[js.Dynamic]
+      val Break = typings.quill.mod.default.`import`("blots/break").asInstanceOf[js.Dynamic]
+      val Embed = typings.quill.mod.default.`import`("blots/embed").asInstanceOf[js.Dynamic]
+      val Block = typings.quill.mod.default.`import`("blots/block").asInstanceOf[js.Dynamic]
+
+      val lineBreakMatcher:js.Function0[js.Any] = () => {
+        val newDelta = js.Dynamic.newInstance(Delta)()
+        newDelta.insert(js.Dynamic.literal(break = ""))
+        newDelta
+      }
+
+
+      val handleEnter = js.ThisFunction.fromFunction3((self:js.Dynamic,range:typings.quill.mod.RangeStatic,context:js.Any) => {
+        if (range.length > 0) {
+          self.quill.scroll.deleteAt(range.index, range.length);  // So we do not trigger text-change
+        }
+
+        val contextFormat = context.asInstanceOf[js.Dynamic].format.asInstanceOf[js.Dictionary[js.Any]]
+
+
+        val lineFormats:js.Dictionary[js.Any] = js.Object.keys(contextFormat.asInstanceOf[js.Object]).foldLeft(js.Dictionary[js.Any]())((lineFormats:js.Dictionary[js.Any], format) => {
+          if (Parchment.query(format, Parchment.Scope.BLOCK).asInstanceOf[Boolean] && !js.Array.isArray(contextFormat(format))) {
+            lineFormats(format) = contextFormat(format);
+          }
+          lineFormats
+        })
+
+
+        self.quill.insertText(range.index, "\n", lineFormats, Sources.user)
+        self.quill.setSelection(range.index + 1, Sources.silent)
+
+
+
+      })
+
+
+      val handleLinebreak = js.ThisFunction.fromFunction3((self:js.Dynamic,range:typings.quill.mod.RangeStatic,context:js.Any) => {
+        self.quill.insertEmbed(range.index, "break", true, Sources.user)
+        val nextChar = self.quill.getText(range.index + 2, 1)
+
+        logger.debug(s"nextChar: $nextChar, length: ${nextChar.asInstanceOf[String].length}")
+        if(nextChar.asInstanceOf[String].length > 0 && nextChar.charCodeAt(0).asInstanceOf[Int] == 10) {
+          logger.debug(s"nextCharCode: ${nextChar.charCodeAt(0).asInstanceOf[Int]}")
+        }
+
+        if(nextChar.asInstanceOf[String].length == 0) {
+          self.quill.insertEmbed(range.index, "break", true, Sources.user)
+        }
+        self.quill.setSelection(range.index + 1, Sources.silent);
+      })
+
+      val options = QuillOptionsStatic()
         .setPlaceholder(field.placeholder.getOrElse(""))
         .setTheme("snow")
         .setDebug("debug")
         .setModules(StringDictionary(
-          "toolbar" -> toolbar
+          "toolbar" -> toolbar,
+          "clipboard" -> js.Dictionary("matchers" -> js.Array(js.Array("BR",lineBreakMatcher))),
+          "keyboard" -> js.Dynamic.literal(
+            "bindings" -> js.Dynamic.literal(
+              "handleEnter" -> js.Dynamic.literal(
+                "key" -> 13,
+                "handler" -> handleEnter
+              ),
+              "linebreak" -> js.Dynamic.literal(
+                "key" -> 13,
+                "shiftKey" -> true,
+                "handler" -> handleLinebreak
+              )
+            )
+          )
         ))
-      )
+
+      Break.prototype.insertInto = js.ThisFunction.fromFunction3((self:js.Any,parent:js.Any, ref:js.Any) => {
+        Embed.prototype.insertInto.call(self, parent, ref)
+      })
+
+      Break.prototype.length= () => 1
+      Break.prototype.value= () => "\n"
+//      Break.prototype.blotName = "break"
+//      Break.prototype.tagName = "BR"
+
+
+      val editor = new typings.quill.mod.default(container,options)
 
       editor.root.innerHTML = prop.get.string
 
