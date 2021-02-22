07713884f118e90dc21f11ccd4d45d9f5eeaf9a4
Andrea Minetti <andrea.minetti@wavein.ch>
2021-02-22 15:02:58 +0100

    Fixed label key

--- db/migrations/BOX_V19__label_natural_key.sql
+++ db/migrations/BOX_V19__label_natural_key.sql
@@ -1,7 +1,7 @@
 
-alter table box.labels drop constraint labels_pkey;
+alter table box.labels drop constraint if exists labels_pkey;
 
-alter table box.labels drop column id;
+alter table box.labels drop column if exists id;
 
 alter table box.labels
     add constraint labels_pkey
